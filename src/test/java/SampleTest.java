8c678b5201af204836af95ed596e666f71d02441
euroksi <153302244+euroksi@users.noreply.github.com>
2025-04-22 10:57:56 +0200

    Add second test in SampleTest

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -4,4 +4,9 @@ public class SampleTest {
         int a = 2 + 2;
         org.junit.jupiter.api.Assertions.assertEquals(4, a);
     }
+    @org.junit.jupiter.api.Test
+    public void testMultiply() {
+        int result = 3 * 3;
+        org.junit.jupiter.api.Assertions.assertEquals(9, result);
+    }
 }
