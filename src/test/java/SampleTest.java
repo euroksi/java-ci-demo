29ced3fbc644db1268356ae4e6f3153bb1d28ca6
euroksi <153302244+euroksi@users.noreply.github.com>
2025-04-22 10:53:46 +0200

    Initial commit with CI setup

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,7 @@
+public class SampleTest {
+    @org.junit.jupiter.api.Test
+    public void testSum() {
+        int a = 2 + 2;
+        org.junit.jupiter.api.Assertions.assertEquals(4, a);
+    }
+}
